e8b4ea52afa24d707f529501c3f47a0e69851f81
Uriel Federez <uafederez@gmail.com>
2022-11-22 23:01:17 +0800

    CORS resolution

--- api.py
+++ api.py
@@ -1,10 +1,12 @@
 from flask import Flask, request
 from flask_restful import Api, Resource
+from flask_cors import CORS
 from resources.LinearModelUnivariate import LinearModelUnivariate
 from resources.LinearModelMultivariate import LinearModelMultivariate
 
-app = Flask(__name__)
-api = Api(app)
+app  = Flask(__name__)
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+api  = Api(app)
 
 api.add_resource(LinearModelUnivariate, '/api/linear-regression-uni')
 api.add_resource(LinearModelMultivariate, '/api/linear-regression-mul')
@@ -13,6 +15,7 @@ api.add_resource(LinearModelMultivariate, '/api/linear-regression-mul')
 #api.add_resource(LinearModelAPI, '/neural-network-bin')
 #api.add_resource(LinearModelAPI, '/neural-network-bin')
 #api.add_resource(LinearModelAPI, '/decision-trees')
+#api.add_resource(LinearModelAPI, '/convnet')
 #api.add_resource(LinearModelAPI, '/kmeans')
 
 if __name__ == "__main__":
