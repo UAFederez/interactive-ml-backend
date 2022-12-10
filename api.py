8729a7800f6420cd6cc75c1e7ef52d121effb077
Uriel Federez <uafederez@gmail.com>
2022-12-10 22:38:12 +0800

    Added API endpoint for decision trees

--- api.py
+++ api.py
@@ -6,6 +6,7 @@ from resources.LinearModelMultivariate import LinearModelMultivariate
 from resources.LogisticRegression import LogisticRegression
 from resources.NeuralNetworks import NeuralNetwork
 from resources.KMeansClustering import KMeansClustering
+from resources.DecisionTrees import DecisionTrees
 
 app  = Flask(__name__)
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
@@ -16,7 +17,7 @@ api.add_resource(LinearModelMultivariate, '/api/linear-regression-mul')
 api.add_resource(LogisticRegression, '/api/logistic-regression')
 api.add_resource(NeuralNetwork, '/api/neural-network')
 api.add_resource(KMeansClustering, '/api/kmeans-clustering')
-#api.add_resource(LinearModelAPI, '/decision-trees')
+api.add_resource(DecisionTrees, '/api/decision-trees')
 
 if __name__ == "__main__":
     app.run(debug = True)
